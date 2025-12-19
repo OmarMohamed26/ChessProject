0a752e74de4fee32efa84584efd16e26b31f708a
Omar Mohamed <es.omar.mostafa2024@alexu.edu.eg>
2025-12-19 21:59:37 +0200

    I don't know why this bug is happeening but try to fix it

--- main.c
+++ main.c
@@ -44,7 +44,8 @@ int main(void)
     SetTargetFPS(60);
     InitializeBoard();
 
-    char standard_game[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    // char standard_game[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    char standard_game[] = "8/8/8/8/8/8/8/R1N1K2k";
     ReadFEN(standard_game, strlen(standard_game));
 
     bool showFps = false;
